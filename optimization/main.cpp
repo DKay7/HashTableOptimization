b8e7b752beca48baf5dc367f16225333ed248611
Danny <bublikplushka@yandex.ru>
2022-05-08 01:23:34 +0300

    updated main, added info into README

---
+++ optimization/main.cpp
@@ -0,0 +1,40 @@
+#include "hash_functions_to_test.h"
+#include "hashtable.h"
+#include "utilities.h"
+#include "texlib.h"
+#include <stdio.h>
+#include <locale.h>
+#include <assert.h>
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+bool key_equality (char* first, char* second);
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+int main()
+{
+    Text csv_data = {};
+    TextCtor (&csv_data, "../data/en-ru.csv");
+    const size_t idx_arr_size = 256;
+    int* idx_for_search_array = (int*) calloc (idx_arr_size, sizeof(*idx_for_search_array));
+
+    HashTable<char*, char*> ht = {};
+    HashTableCtor<char*, char*> (&ht, 50000, polynimial_rolling_hash, key_equality);
+
+    FillIndexesArrayWithRandomValues (idx_for_search_array, idx_arr_size, csv_data.non_empty_lines);
+
+    FillHashTableFromStrCSV (&csv_data, &ht);
+
+    SearchSpeedTest (idx_for_search_array, idx_arr_size, &ht, &csv_data);
+
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+bool key_equality (char* first, char* second)
+{
+    return (strcmp (first, second) == 0);
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
